b71572eb07f115fbb26cad6d0de86aaceeaca3c5
JP Hastings-Spital <jphastings@gmail.com>
2016-01-11 19:46:07 +0000

    Count unknown authors in search results

--- lib/models/cred_key.go
+++ lib/models/cred_key.go
@@ -40,9 +40,9 @@ func SearchCredKeysBreakdown(key string) []*credence.SearchResult_KeyBreakdown {
   var dbSpecificSelect string
   switch cfg.DB.Type {
   case "postgres":
-    dbSpecificSelect = "k.key, r.statement_hash, sum(r.no_comment::int * a.weight), sum(r.is_true::int * a.weight), sum(r.is_false::int * a.weight), sum(r.is_ambiguous::int * a.weight), sum(coalesce(a.weight, 1))"
+    dbSpecificSelect = "k.key, r.statement_hash, sum(r.no_comment::int * coalesce(a.weight, 1)), sum(r.is_true::int * coalesce(a.weight, 1)), sum(r.is_false::int * coalesce(a.weight, 1)), sum(r.is_ambiguous::int * coalesce(a.weight, 1)), sum(coalesce(a.weight, 1))"
   case "sqlite3":
-    dbSpecificSelect = "k.key, r.statement_hash, sum(r.no_comment * a.weight), sum(r.is_true * a.weight), sum(r.is_false * a.weight), sum(r.is_ambiguous * a.weight), sum(coalesce(a.weight, 1))"
+    dbSpecificSelect = "k.key, r.statement_hash, sum(r.no_comment * coalesce(a.weight, 1)), sum(r.is_true * coalesce(a.weight, 1)), sum(r.is_false * coalesce(a.weight, 1)), sum(r.is_ambiguous * coalesce(a.weight, 1)), sum(coalesce(a.weight, 1))"
   }
 
   rows, err := db.
