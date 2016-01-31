1668aed862de14301824f2dc329cef47002e8007
JP Hastings-Spital <jphastings@gmail.com>
2016-01-31 23:45:08 +0000

    Layout tweaks

--- lib/web/view_models/identity_assertion.go
+++ lib/web/view_models/identity_assertion.go
@@ -2,6 +2,7 @@ package viewModels
 
 import (
   "net/url"
+  "encoding/hex"
   "github.com/jphastings/credence/lib/models"
   "github.com/jphastings/credence/lib/definitions/credence"
 )
@@ -11,23 +12,34 @@ type IdentityAssertionProps struct {
 
   Name string
   Identity DisplayedUri
+  Proof DisplayedUri
   LatestCreds []Props
+  Fingerprint string
 }
 
 func RetrieveIdentityAssertion(identityAssertion *credence.IdentityAssertion) IdentityAssertionProps {
-	var creds []Props
-	latestCreds := models.CredRecordSample(identityAssertion.Fingerprint, 15)
-	for _, credRecord := range latestCreds {
-		creds = append(creds, RetrieveCredRecord(credRecord))
-	}
+  var creds []Props
+  // TODO: I don't think this fingerprint is actually the correct identity…
+  latestCreds := models.CredRecordSample(identityAssertion.Fingerprint, 15)
+  for _, credRecord := range latestCreds {
+    creds = append(creds, RetrieveCredRecord(credRecord))
+  }
 
-	identityUri, _ := url.Parse(identityAssertion.IdentityUri)
+  proofUri, _ := url.Parse(identityAssertion.ProofUri)
+  identityUri, _ := url.Parse(identityAssertion.IdentityUri)
   props := IdentityAssertionProps{
     Name: identityAssertion.Name,
     Identity: DisplayedUri{
-    	String: identityUri.Host,
-    	Url: identityAssertion.IdentityUri,
+      String: identityUri.Host,
+      Url: identityAssertion.IdentityUri,
     },
+    Proof: DisplayedUri{
+      String: proofUri.Host,
+      Url: identityAssertion.ProofUri,
+    },
+    // TODO: Find better way of representing this
+    Fingerprint: hex.EncodeToString(identityAssertion.Fingerprint),
+    
     LatestCreds: creds,
   }
   return props
